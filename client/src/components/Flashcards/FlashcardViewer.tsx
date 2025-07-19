f96d5e00d67bfa7054ee527f079f7683b56e77c4
Aryaman <aryamanpandey22@gmail.com>
2025-07-19 15:57:07 -0300

    Completed the Flashcard Viewer Page.

--- client/src/components/Flashcards/FlashcardViewer.tsx
+++ client/src/components/Flashcards/FlashcardViewer.tsx
@@ -8,7 +8,8 @@ CardContent,
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
-
+import ResponseButtons from "@/components/Flashcards/ResponseButtons";
+import NavigationButtons from "@/components/Flashcards/NavigationButtons";
 
 export default function FlashcardViewer() {
     const [showAnswer, setShowAnswer] = useState(false);
@@ -17,22 +18,26 @@ export default function FlashcardViewer() {
         <div 
         className="w-full max-w-xl m-5"
         style={{perspective: 1000}}
-        onClick={() => setShowAnswer(prev => !prev)}>
+        >
 
             <motion.div
                 animate={{rotateY: showAnswer ? -180: 0}}
                 transition={{duration: 0.3}}
                 className="relative w-full aspect-[5/3] cursor-pointer"
                 style={{
-                    transformStyle: "preserve-3d"
+                    transformStyle: "preserve-3d",
+                    margin: "20"
                 }}
+                onClick={() => setShowAnswer(prev => !prev)}
             >
                 <FlashcardQuestion className="absolute"/>
 
                 <FlashcardAnswer className="absolute" />
 
             </motion.div>
-
+            
+            <ResponseButtons showAnswer={showAnswer}/>
+            
         </div>
     );
 }
@@ -48,7 +53,7 @@ function FlashcardQuestion({className }: {className? : string}) {
                 <CardTitle>Question</CardTitle>
             </CardHeader>
             <CardContent className="flex justify-center items-center w-full grow">
-                <div className="lg:text-xl font-semibold sm:text-sm">What is a flashcard?</div>
+                <div className="md:text-xl font-semibold sm:text-sm">What is a flashcard?</div>
             </CardContent>
         </Card> 
     );
@@ -66,7 +71,7 @@ function FlashcardAnswer({className} : {className? : string}) {
                 <CardTitle>Answer</CardTitle>
             </CardHeader>
             <CardContent className="flex justify-center items-center grow">
-                <div className="lg:text-xl break-words font-medium sm:text-sm">A flashcard is a note to test your knowledge. You write the question on one side, and the answer on the other. you flip it to find the answer</div>
+                <div className="md:text-xl break-words font-medium sm:text-sm">A flashcard is a note to test your knowledge. You write the question on one side, and the answer on the other. you flip it to find the answer</div>
             </CardContent>
         </Card>  
     );
---
+++ client/src/components/Flashcards/ResponseButtons.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button"
+import {motion} from "framer-motion";
+
+const MotionButton = motion(Button);
+
+export default function ResponseButtons({showAnswer} : {showAnswer: boolean}) {
+    return(
+        <motion.div
+            animate={{opacity: showAnswer ? 1 : 0}}
+            transition={{duration: 0.3}}
+            className="flex gap-5 justify-center text-white mt-5"
+        >
+            <MotionButton 
+                className="px-10 bg-green-500 hover:bg-green-600"
+                whileTap={{scale: 0.8}}
+            >
+                Got it
+            </MotionButton>
+            <MotionButton 
+                className="px-8 bg-red-500 hover:bg-red-600"
+                whileTap={{scale:0.8}}
+            >
+                Missed it
+            </MotionButton>
+        </motion.div>
+    )
+}
+
+function correctButton() {
+
+}
+
+function incorrectButton() {
+
+}
