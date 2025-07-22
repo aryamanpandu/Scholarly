903ee90f4d41566e74aa42c6f87e472d27257f97
Aryaman <aryamanpandey22@gmail.com>
2025-07-22 03:48:37 -0300

    Added Page Transitions to the App

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { ReactNode } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 import "@/pages/App.css";
 import LoginPage from "@/pages/LoginPage";
 import Home from "@/pages/Home";
@@ -6,25 +9,103 @@ import SignUpPage from "@/pages/SignUpPage";
 import DecksHome from "@/pages/DecksHome";
 import FlashcardsHome from "@/pages/FlashcardsHome";
 
-import FlashcardNavBar from "@/components/Flashcards/FlashcardNavBar";
+import NavBar from "@/components/NavBar";
 import FlashcardViewerPage from "@/pages/FlashcardViewerPage";
 
-function App() {
-  return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element= {<LoginPage />} />
-        <Route path="/signup" element= {<SignUpPage />} /> 
-        <Route path="/home" element= { <Home /> } />
-        <Route path="/login" element= { <LoginPage /> } />
-        <Route path="/deckHome/:topicId" element= { <DecksHome /> } />
-        <Route path="/flashcardHome/:deckId" element= { <FlashcardsHome /> } />
-        <Route path="/flashNav" element= {<FlashcardNavBar />} />
-        <Route path="/flashcardViewer" element= {<FlashcardViewerPage/>} />
+import { Toaster } from "sonner";
+
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return(
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route 
+          path="/login"
+          element={
+            <PageWrapper>
+              <LoginPage/>
+            </PageWrapper>
+          }
+        />
+
+        <Route 
+          path="/signup"
+          element={
+            <PageWrapper>
+              <SignUpPage/>
+            </PageWrapper>
+          }
+        />
+
+        <Route 
+          path="/home"
+          element={
+            <PageWrapper>
+              <Home/>
+            </PageWrapper>
+          }
+        />
+
+        <Route 
+          path="/deckHome/:topicId"
+          element={
+            <PageWrapper>
+              <DecksHome/>
+            </PageWrapper>
+          }
+        />
+
+        <Route 
+          path="/flashcardHome/:deckId"
+          element={
+            <PageWrapper>
+              <FlashcardsHome/>
+            </PageWrapper>
+          }
+        />
+
+        <Route 
+          path="/flashcardViewer"
+          element={
+            <PageWrapper>
+              <FlashcardViewerPage/>
+            </PageWrapper>
+          } 
+        />
       </Routes>
-    </BrowserRouter>
-    </>
+    </AnimatePresence>
+  )
+}
+
+function PageWrapper({children}: {children: ReactNode}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0}}
+      transition={{ 
+        duration: 0.17, 
+        ease: "easeInOut" 
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+ }
+
+function App() {
+  const location = useLocation();
+
+  const hideNavBar = ["/login", "/signup"].includes(location.pathname);
+  return (
+    <div>
+      <Toaster expand={true} position='bottom-center' richColors/>
+      {!hideNavBar && <NavBar isLoggedIn={true}/>}
+      <AnimatedRoutes />
+
+    </div>
   );
 }
 
