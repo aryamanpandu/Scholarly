00d15c31277a937e67b2e30910ce085afd2e559d
Aryaman <aryamanpandey22@gmail.com>
2025-08-15 22:05:27 -0300

    Update Landing Page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,15 +2,24 @@ import { Button } from "@/components/ui/button";
 
 export default function LandingPage() {
     return (
-        <div className="bg-gray-200 h-screen">  
-            <div className="text-5xl">
-                Master Anything. One Flashcard at a Time.
+        <>
+            <div className="bg-gray-200 h-screen flex justify-center items-center flex-col">  
+                <div className="text-5xl">
+                    Master Anything.
+                </div>
+                <div className="text-5xl">
+                    One Flashcard at a Time.
+                </div>
+                <div>
+                    <Button>Get Started</Button>
+                    <div>
+                        Learn more <i className="bi bi-arrow-down"></i>
+                    </div>
+                </div>
+                
             </div>
-            <Button>Get Started</Button>
-            <div>
-                Learn more <i className="bi bi-arrow-down"></i>
-            </div>
-        </div>
+        </>
+        
     );
 }
 
